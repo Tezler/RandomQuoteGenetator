65ad812f128a0f69ed63919ccf1e476ca572d4a5
Keontarius Clark <keontariusclark@icloud.com>
2017-09-19 18:27:02 -0700

    Clean Up.
    Updated and cleaned up noted. removed unused variables

--- js/script.js
+++ js/script.js
@@ -2,22 +2,20 @@
 // quotesCopy is a splice of the original object array - used for preventing repeating.
 // randomNumber contains the number generated within the function getRandomQuote
 // quoteOutput stores the values that are going to be returned in getRandomQuote and printed.
-// bgColor contains the color that the background will change to once the printQuote function is activated.
 var autoGenerateQuote = window.setInterval(printQuote, 30000);
 var quotesCopy = quotes.slice(0, quotes.length);
 var randomNumber;
 var quoteOutput;
-var bgColor;
 
-// event listener to respond to "Show another quote" button clicks.
-// when user clicks anywhere on the button, the "printQuote" function is called.
+/* event listener to respond to "Show another quote" button clicks.
+ when user clicks anywhere on the button, the "printQuote" function is called. */
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-// runs the printQuote function on page load.
+// runs the printQuote function on page body load.
 document.getElementById("body").addEventListener("load", printQuote, true);
 
-// Prints a quote to the page & changes background color by calling the gerRandomQuote function and returning the value.
-// added log to confirm that quotes are not repeating(project requirement) and a reset for the autoGenerateQuote interval.
+/* Prints a quote to the page & changes background color by calling the getRandomQuote function and returning the value.
+ added a log to confirm that quotes are not repeating(project requirement) and a reset for the autoGenerateQuote interval. */
 function printQuote() {
   clearInterval(autoGenerateQuote);
   autoGenerateQuote = window.setInterval(printQuote, 30000);
@@ -47,8 +45,8 @@ function getRandomColor() {
   return color;
 }
 
-/*Splices an object from the original object array, stores it in a new variable then returns it
-for printQuote to use. Added a check to refill the new object onces it becomes empty*/
+/*Splices an object from the quotesCopy object array, stores it in a new variable then returns it
+for printQuote to use. Added a check to refill the quotesCopy object onces it becomes empty*/
 function getRandomQuote() {
   if (quotesCopy.length == 0) {
     quotesCopy = quotes.slice(0, quotes.length);
